76e49b15160639dcee179b4ad45040cd88ae141f
kk2115 <80417519+kk2115@users.noreply.github.com>
2022-10-06 11:00:49 +0200

    init commit

---
+++ Core/Inc/RFID.h
@@ -0,0 +1,42 @@
+/*
+ * RFID.h
+ *
+ *  Created on: Oct 5, 2022
+ *      Author: studia
+ */
+
+#ifndef INC_RFID_H_
+#define INC_RFID_H_
+
+typedef enum{
+	ERROR,
+	DONE_CORRECTLY
+}MFRC522_Operation_Status;
+
+
+/***********************************************************
+ *Initialization of the MFRC522 module
+ ***********************************************************/
+extern MFRC522_Operation_Status RFID_init_MFRC522();
+
+extern MFRC522_Operation_Status Request_Card_MFRC522();
+
+/***********************************************************
+ * Function is combining anti-colision process and card activation,
+ * since this library for know does not handle multi card connection
+ * @return pointer to a 4 bytes long table with NUID
+ ***********************************************************
+extern uint8_t* Anticollision_UID_MFRC522();
+
+extern MFRC522_Operation_Status Authorization_Card_MFRC522();
+
+/***********************************************************
+ * The funckcion is checking chosen block in card memory
+ * @return Pointer to 16 bytes long table with blocks data
+ * *********************************************************/
+extern uint8_t* Check_Block_MFRC522(uint8_t blockNumber);
+
+
+extern void Write_Register_MFRC522(uint8_t adress, uint8_t value);
+extern uint8_t* Read_Register_MFRC522(uint8_t adress);
+#endif /* INC_RFID_H_ */
---
+++ Core/Src/RFID.c
@@ -0,0 +1,8 @@
+/*
+ * RFID.c
+ *
+ *  Created on: Oct 5, 2022
+ *      Author: studia
+ */
+
+
